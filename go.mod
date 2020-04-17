90dca2a0b70b40fe0efa3947cb7604dbab223e8e
jackyzha0 <j.zhao2k19@gmail.com>
2020-04-16 23:17:08 -0700

    basic cli

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/jackyzha0/nanoDB
 go 1.13
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/google/go-cmp v0.4.0
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/afero v1.2.2
 	github.com/stretchr/testify v1.2.2
+	github.com/urfave/cli/v2 v2.2.0
 )
--- main.go
+++ main.go
@@ -1,16 +1,131 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/jackyzha0/nanoDB/api"
 	"github.com/jackyzha0/nanoDB/index"
 	"github.com/jackyzha0/nanoDB/log"
+
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
+	app := &cli.App{
+		Name:  "nanodb",
+		Usage: "a simple, easy, and stupid database for prototyping and hackathons",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:        "port",
+				Aliases:     []string{"p"},
+				Value:       3000,
+				Usage:       "port to run nanodb on",
+				DefaultText: "3000",
+			},
+			&cli.StringFlag{
+				Name:        "dir",
+				Aliases:     []string{"d"},
+				Value:       "db",
+				Usage:       "directory to look for keys",
+				DefaultText: "db",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "start",
+				Aliases: []string{"st"},
+				Usage:   "start a nanodb server",
+				Action: func(c *cli.Context) error {
+					return serve(c.Int("port"), c.String("dir"))
+				},
+			}, {
+				Name:    "shell",
+				Aliases: []string{"sh"},
+				Usage:   "start an interactive nanodb shell",
+				Action: func(c *cli.Context) error {
+					// TODO: implement shell
+					/*
+						index
+						lookup <key>
+						delete <key>
+					*/
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// serve defines all the endpoints and starts a new http server on :3000
+func serve(port int, dir string) error {
 	log.SetLoggingLevel(log.INFO)
+	setup(dir)
+
+	router := httprouter.New()
+
+	// define endpoints
+	router.GET("/", api.GetIndex)
+	router.POST("/", api.RegenerateIndex)
+	router.GET("/:key", api.GetKey)
+	router.GET("/:key/:field", api.GetKeyField)
+	router.PUT("/:key", api.UpdateKey)
+	router.DELETE("/:key", api.DeleteKey)
+	router.PATCH("/:key/:field", api.PatchKeyField)
+
+	// start server
+	log.Info("starting api server on port %d", port)
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), router)
+}
+
+func getLockLocation(dir string) string {
+	base := "nanodb_lock"
+	if dir == "" || dir == "." {
+		return base
+	}
+	return dir + "/" + base
+}
+
+func setup(dir string) {
 	log.Info("initializing nanoDB")
-	index.I = index.NewFileIndex("db")
+	index.I = index.NewFileIndex(dir)
 	index.I.Regenerate()
 
-	api.Serve()
+	// create nanodb lock
+	_, err := index.I.FileSystem.Create(getLockLocation(dir))
+	if err != nil {
+		log.Warn("couldn't acquire lock on %s", dir)
+		log.Fatal(err)
+		return
+	}
+
+	// trap sigint
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		cleanup(getLockLocation(dir))
+		os.Exit(1)
+	}()
+}
+
+func cleanup(lockdir string) {
+	log.Info("caught term signal! cleaning up...")
+
+	err := index.I.FileSystem.Remove(lockdir)
+	if err != nil {
+		log.Warn("couldn't remove lock")
+		log.Fatal(err)
+		return
+	}
 }
